5ddf951731dbfb8c2bf6dc41cadec61dfc79c076
Julia Grib <yulgribgmail.com>
2022-11-06 22:03:06 +0300

    fix: complete third task(1)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 
 fs.readdir(path.join(__dirname, './secret-folder'), (err, data) => {
     data.forEach(file => {
-        if (fs.statSync(path.join(__dirname, 'secret-folder', file)).isFile()) {
-            let name = path.basename(file);
-            let dot = name.indexOf('.');
-            let permission = path.extname(file);
-            let size = fs.statSync(path.join(__dirname, 'secret-folder', file)).size;
-            console.log(`${name.slice(0, dot)} - ${permission.slice(1)} - ${size/ 1024}kb`);
-        }
+        fs.stat(path.join(__dirname, 'secret-folder', file), function(err, stats) {
+            if (!(stats.isDirectory())) {
+                let name = path.basename(file);
+                let dot = name.indexOf('.');
+                let permission = path.extname(file);
+                let size = stats["size"];
+                console.log(`${name.slice(0, dot)} - ${permission.slice(1)} - ${size/ 1024}kb`);
+                }
+        });
+
     })
 });
-
